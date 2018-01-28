2f808baf055f19c2acdcf0ffd069399923a42489
Otto Kivikärki <otto.kivikarki@vincit.fi>
2018-01-28 20:09:10 +0200

    Clean environment scripts.

--- docker/start_app.sh
+++ docker/start_app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Allow running the script from parent folders by getting the path and cd'ing to the folder
 SCRIPT_PATH="`dirname \"$0\"`"
 cd ${SCRIPT_PATH}
 
--- docker/wait_for_mongo.sh
+++ docker/wait_for_mongo.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-host="$1"
-shift
-cmd="$@"
-
 # Get the active docker process id
 DOCKER_ID=$(docker-compose ps -q mongodb)
 
 # Exec into the running container and wait until mongodb is responding
-until docker exec -i -t "${DOCKER_ID}" mongo --quiet --host "${host}" --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)'; do
+until docker exec -i -t "${DOCKER_ID}" mongo --quiet --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)'; do
+  echo 'waiting for db..'
   sleep 1
 done
